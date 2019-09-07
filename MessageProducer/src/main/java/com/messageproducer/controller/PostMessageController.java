7c4c73fb7e4d2672d65ef7dc86741e4622354d9d
raul1rnjn535_3 <ranjanraul10@gmail.com>
2019-09-07 17:59:08 +0530

    Dockerized the application

--- MessageProducer/src/main/java/com/messageproducer/controller/PostMessageController.java
+++ MessageProducer/src/main/java/com/messageproducer/controller/PostMessageController.java
@@ -8,6 +8,7 @@ import javax.jms.JMSException;
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.messageproducer.model.TransactionMessage;
 import com.messageproducer.producer.ProduceMessage;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/gateway/publish")
 @Component
--- MessageProducer/src/test/java/com/messageproducer/MessageProducerApplicationTests.java
+++ MessageProducer/src/test/java/com/messageproducer/MessageProducerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.messageproducer;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
